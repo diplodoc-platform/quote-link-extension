06ba3f82ccb3b15532d73f660587b226490e967f
kseniyakuzina <kseniyakuzina@yandex-team.ru>
2025-02-17 19:38:14 +0300

    feat(quoteLink): add quote link plugin

---
+++ tests/src/plugin.test.ts
@@ -0,0 +1,243 @@
+import transform from '@diplodoc/transform';
+import dd from 'ts-dedent';
+import MarkdownIt from 'markdown-it';
+import attrs from 'markdown-it-attrs';
+
+import * as quoteLinkExtension from '../../src/plugin';
+
+const html = (text: string, opts?: quoteLinkExtension.TransformOptions) => {
+    const {result} = transform(text, {
+        plugins: [quoteLinkExtension.transform({bundle: false, ...opts})],
+    });
+
+    return result.html;
+};
+
+const meta = (text: string, opts?: quoteLinkExtension.TransformOptions) => {
+    const {result} = transform(text, {
+        plugins: [quoteLinkExtension.transform({bundle: false, ...opts})],
+    });
+
+    return result.meta;
+};
+
+const parse = (text: string, opts?: quoteLinkExtension.TransformOptions) => {
+    const md = new MarkdownIt().use(quoteLinkExtension.transform({bundle: false, ...opts}));
+    md.use(attrs, {leftDelimiter: '{', rightDelimiter: '}'});
+    return md.parse(text, {});
+};
+
+describe('Quote link extension - plugin', () => {
+    it('should render single quote link', () => {
+        expect(
+            html(dd`
+            > [Quote link](https://ya.ru){data-quotelink="true"}
+            >
+            > quote link text
+            `),
+        ).toBe(
+            `<blockquote class="yfm-quote-link">
+<p><a href="https://ya.ru" data-quotelink="true">Quote link</a></p>
+<p>quote link text</p>
+</blockquote>
+`,
+        );
+    });
+
+    it('should render nested quote link', () => {
+        expect(
+            html(dd`
+            > [Quote link](https://ya.ru){data-quotelink="true"}
+            >
+            > quote link text
+            >
+            > > [Nested](https://nested.ru){data-quotelink="true"}
+            > >
+            > > nested link text
+            `),
+        ).toBe(
+            `<blockquote class="yfm-quote-link">
+<p><a href="https://ya.ru" data-quotelink="true">Quote link</a></p>
+<p>quote link text</p>
+<blockquote class="yfm-quote-link">
+<p><a href="https://nested.ru" data-quotelink="true">Nested</a></p>
+<p>nested link text</p>
+</blockquote>
+</blockquote>
+`,
+        );
+    });
+
+    it('should render simple quote inside quote link', () => {
+        expect(
+            html(dd`
+            > [Quote link](https://ya.ru){data-quotelink="true"}
+            >
+            > quote link text
+            >
+            > > Simple quote
+            > >
+            > > simple quote text
+            `),
+        ).toBe(
+            `<blockquote class="yfm-quote-link">
+<p><a href="https://ya.ru" data-quotelink="true">Quote link</a></p>
+<p>quote link text</p>
+<blockquote>
+<p>Simple quote</p>
+<p>simple quote text</p>
+</blockquote>
+</blockquote>
+`,
+        );
+
+        expect(
+            html(dd`
+            > [Quote link](https://ya.ru){data-quotelink="true"}
+            >
+            > quote link text
+            >
+            > > [Simple quote with link](https://nested.ru)
+            > >
+            > > simple quote text
+            `),
+        ).toBe(
+            `<blockquote class="yfm-quote-link">
+<p><a href="https://ya.ru" data-quotelink="true">Quote link</a></p>
+<p>quote link text</p>
+<blockquote>
+<p><a href="https://nested.ru">Simple quote with link</a></p>
+<p>simple quote text</p>
+</blockquote>
+</blockquote>
+`,
+        );
+    });
+
+    it('should render quote link inside simple quote', () => {
+        expect(
+            html(dd`
+            > Simple quote
+            >
+            > simple quote text
+            >
+            > > [Nested quote link](https://nested.ru){data-quotelink="true"}
+            > >
+            > > nested quote link text
+            `),
+        ).toBe(
+            `<blockquote>
+<p>Simple quote</p>
+<p>simple quote text</p>
+<blockquote class="yfm-quote-link">
+<p><a href="https://nested.ru" data-quotelink="true">Nested quote link</a></p>
+<p>nested quote link text</p>
+</blockquote>
+</blockquote>
+`,
+        );
+
+        expect(
+            html(dd`
+            > [Simple link](https://ya.ru)
+            >
+            > simple link text
+            >
+            > > [Nested quote link](https://nested.ru){data-quotelink="true"}
+            > >
+            > > nested quote link text
+            `),
+        ).toBe(
+            `<blockquote>
+<p><a href="https://ya.ru">Simple link</a></p>
+<p>simple link text</p>
+<blockquote class="yfm-quote-link">
+<p><a href="https://nested.ru" data-quotelink="true">Nested quote link</a></p>
+<p>nested quote link text</p>
+</blockquote>
+</blockquote>
+`,
+        );
+    });
+
+    it('should render simple quote without link', () => {
+        expect(html('> quote text')).toBe(
+            `<blockquote>
+<p>quote text</p>
+</blockquote>
+`,
+        );
+
+        expect(
+            html(dd`
+        > [Simple link](https://ya.ru)
+        >
+        > link text
+        `),
+        ).toBe(
+            `<blockquote>
+<p><a href="https://ya.ru">Simple link</a></p>
+<p>link text</p>
+</blockquote>
+`,
+        );
+    });
+
+    it('should not add assets to meta if no yfm-quote-link is found', () => {
+        expect(meta('paragraph')).toBeUndefined();
+    });
+
+    it('should add default assets to meta', () => {
+        expect(
+            meta(dd`
+            > [Quote link](https://ya.ru){data-quotelink="true"}
+            >
+            > quote link text
+            `),
+        ).toStrictEqual({
+            script: ['_assets/quote-link-extension.js'],
+            style: ['_assets/quote-link-extension.css'],
+        });
+    });
+
+    it('should add custom assets to meta', () => {
+        expect(
+            meta(
+                dd`
+                > [Quote link](https://ya.ru){data-quotelink="true"}
+                >
+                > quote link text
+                `,
+                {runtime: 'yfm-quote-link'},
+            ),
+        ).toStrictEqual({
+            script: ['yfm-quote-link'],
+            style: ['yfm-quote-link'],
+        });
+    });
+
+    it('should add custom assets to meta 2', () => {
+        expect(
+            meta(
+                dd`
+                > [Quote link](https://ya.ru){data-quotelink="true"}
+                >
+                > quote link text
+                `,
+                {
+                    runtime: {script: 'yfm-quote-link.script', style: 'yfm-quote-link.style'},
+                },
+            ),
+        ).toStrictEqual({script: ['yfm-quote-link.script'], style: ['yfm-quote-link.style']});
+    });
+
+    it('should parse markup with quote link to token stream', () => {
+        expect(
+            parse(dd`
+            > [Quote link](https://ya.ru){data-quotelink="true"}
+            >
+            > quote link text
+            `),
+        ).toMatchSnapshot();
+    });
+});
