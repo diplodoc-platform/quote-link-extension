ccf18a54d4330f271b72fbf403c75c6bb3180cf2
d3m1d0v <d3m1d0v@yandex-team.ru>
2025-02-28 14:06:17 +0300

    refactor(plugin): improve code of plugin

--- src/plugin/helpers.ts
+++ src/plugin/helpers.ts
@@ -4,24 +4,56 @@ const matchCloseToken = (tokens: MdIt.Token[], i: number) => {
     return tokens[i].type === 'blockquote_close';
 };
 
-export const matchOpenToken = (tokens: MdIt.Token[], i: number) => {
+const matchOpenToken = (tokens: MdIt.Token[], i: number) => {
     return tokens[i].type === 'blockquote_open';
 };
 
-export function findCloseTokenIdx(tokens: MdIt.Token[], idx: number) {
+export function matchBlockquote(tokens: MdIt.Token[], idx: number) {
+    if (!matchOpenToken(tokens, idx)) {
+        return null;
+    }
+
     let level = 0;
-    let i = idx;
+    let i = idx + 1;
     while (i < tokens.length) {
         if (matchOpenToken(tokens, i)) {
             level++;
         } else if (matchCloseToken(tokens, i)) {
             if (level === 0) {
-                return i;
+                return {
+                    openToken: tokens[idx],
+                    closeToken: tokens[i],
+                    closeTokenIndex: i,
+                };
             }
             level--;
         }
 
         i++;
+    }
+
+    return null;
+}
+
+export function matchLinkAtInlineStart(inlineToken: MdIt.Token) {
+    if (inlineToken.type !== 'inline' || !inlineToken.children?.length) {
+        return null;
+    }
+
+    const {children: tokens} = inlineToken;
+    if (tokens[0].type !== 'link_open') {
+        return null;
+    }
+
+    for (let i = 0; i < tokens.length; i++) {
+        const token = tokens[i];
+        if (token.type === 'link_close') {
+            return {
+                openToken: tokens[0],
+                closeToken: token,
+                closeTokenIndex: i,
+            };
+        }
     }
 
     return null;
